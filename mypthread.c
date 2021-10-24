6065d739aa8b0c54880052793a41ea95672812a6
Rohit Rao <rohorao@gmail.com>
2021-10-24 14:51:21 -0400

    more changes

--- mypthread.c
+++ mypthread.c
@@ -69,14 +69,14 @@ void unblockThread(mypthread_t tid){
         temp = temp->next;
     }
 }
-int isFinished(mypthread_t tid){
+bool isFinished(mypthread_t tid){
     threadNode* temp = ThreadQueue;
     while(temp != NULL){
-        if(temp->thread->threadID == tid &&temp->thread->threadStatus == done)
-            return 1;
+        if(temp->thread->threadID == tid && temp->thread->threadStatus == done)
+            return true;
         temp = temp->next;
     }
-    return 0;
+    return false;
 }
 tcb* getTCB(mypthread_t tid){
     threadNode* temp = ThreadQueue;
@@ -87,6 +87,12 @@ tcb* getTCB(mypthread_t tid){
     }
     return NULL;
 }
+void freeNode(threadNode* t){
+    if (t == NULL) return;
+    free(t->thread->threadContext.uc_stack.ss_sp);
+    free(t->thread);
+    free(t);
+}
 void destroyAll(){
     threadNode* prev = NULL;
     threadNode* cur = ThreadQueue;
@@ -94,20 +100,21 @@ void destroyAll(){
         if(cur->thread->threadStatus == destroy){
             prev->next = cur->next;
 			cur = prev->next;
-            free(prev->thread->threadContext.uc_stack.ss_sp);
-            free(prev->thread);
-            free(prev);
+            freeNode(prev);
             continue;
         }
         prev = cur;
         cur = cur->next;
     }
 }
-
-
-// INITAILIZE ALL YOUR VARIABLES HERE
-void freeThreads(void);
-static void sched_stcf();
+void freeThreadQueue(void){
+    while(ThreadQueue != NULL){
+        threadNode* temp = ThreadQueue;
+        ThreadQueue = ThreadQueue->next;
+        freeNode(temp);
+    }
+    free(runningBlock);
+}
 
 
 tcb* create_tcb(mypthread_t tid, bool createContext){
@@ -140,14 +147,14 @@ void resetTimer(){
 
     timer.it_interval.tv_sec = 0;
     timer.it_interval.tv_usec = QUANTUM;
+
     setitimer(ITIMER_PROF, &timer, NULL);
 }
 
 void createMainThread(){
-    atexit(freeThreads);
+    atexit(freeThreadQueue);
     currentThread = threadIDs++;
     runningBlock = create_tcb(currentThread, false);
-    
     resetTimer();
     getcontext(&(runningBlock->threadContext));
 }
@@ -156,10 +163,9 @@ void createMainThread(){
 int mypthread_create(mypthread_t * thread, pthread_attr_t * attr, void *(*function)(void*), void * arg) {
     if(!threadIDs) createMainThread();
 
-    currentThread = threadIDs;
+    currentThread = threadIDs++;
+    *thread = currentThread;
     tcb* threadBlock = create_tcb(currentThread, true);
-    *thread = threadIDs++;
-	threadBlock->elapsedTime = 0;
     enqueue(threadBlock);
     makecontext(&(threadBlock->threadContext), (void*) function, 1, arg); 
     return 0;
@@ -206,10 +212,9 @@ int mypthread_join(mypthread_t thread, void **value_ptr) {
 /* initialize the mutex lock */
 int mypthread_mutex_init(mypthread_mutex_t *mutex, const pthread_mutexattr_t *mutexattr) {
 	//initialize data structures for this mutex
-    if(!threadIDs) createMainThread();
+    if(threadIDs==0) createMainThread();
     mutex->waitList = NULL;
     mutex->lock = 0;
-	
     return 0;
 };
 
@@ -254,533 +259,11 @@ static void sched_stcf() {
     
     tcb* prevThread = runningBlock;
     runningBlock = dequeue();
-    if(runningBlock == NULL){
-        runningBlock = prevThread;
-        return;
+    if(runningBlock != NULL){
+        prevThread->elapsedTime++;
+        enqueue(prevThread);
+        resetTimer();
+        swapcontext(&(prevThread->threadContext), &(runningBlock->threadContext));
     }
-    prevThread->elapsedTime++;
-    enqueue(prevThread);
-    
-    resetTimer();
-    swapcontext(&(prevThread->threadContext), &(runningBlock->threadContext));
+    else runningBlock = prevThread;
 }
-
-void freeThreads(void){
-    while(ThreadQueue != NULL){
-        threadNode* temp = ThreadQueue;
-        ThreadQueue = ThreadQueue->next;
-        free(temp->thread->threadContext.uc_stack.ss_sp);
-        free(temp->thread);
-        free(temp);
-    }
-    free(runningBlock);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // File:	mypthread.c
-
-// // List all group member's name:
-// // username of iLab:
-// // iLab Server:
-
-// #include "mypthread.h"
-
-// // INITAILIZE ALL YOUR VARIABLES HERE
-// // YOUR CODE HERE
-// int threadID = 0;
-// Node* currentlyRunningThread;
-// Node* readyQueue = NULL;
-// Node* sleepingQueue = NULL;
-// Node* finishedQueue = NULL;
-// Node* freeThreads = NULL;
-
-// void destroyAll();
-// static void sched_stcf();
-// static void schedule();
-
-// void resetTimer() {
-// 	struct sigaction action;
-//     struct itimerval timer;
-
-//     memset(&action, 0, sizeof(struct sigaction));
-//     action.sa_handler = &schedule;
-//     sigaction(SIGPROF, &action, NULL);
-
-//     timer.it_value.tv_sec = 0;
-//     timer.it_value.tv_usec = QUANTUM;
-
-//     timer.it_interval.tv_sec = 0;
-//     timer.it_interval.tv_usec = QUANTUM;
-//     setitimer(ITIMER_PROF, &timer, NULL);
-// }
-
-// void runMainThread() {
-// 	atexit(destroyAll);
-// 	threadControlBlock* mainThread = malloc(sizeof(threadControlBlock));
-// 	mainThread->threadID = threadID++;
-// 	mainThread->threadStatus = READY;
-// 	mainThread->sleptByThreadID = -1;
-// 	mainThread->quantumsElapsed = 0;
-// 	mainThread->valuePtr = NULL;
-// 	mainThread->returnValue = NULL;
-
-// 	Node* newBlock = malloc(sizeof(Node));
-// 	newBlock->next = NULL;
-// 	newBlock->threadInfo = mainThread;
-
-// 	currentlyRunningThread = newBlock;
-// 	// currentlyRunningThread->next = NULL;
-// 	// currentlyRunningThread->threadInfo = mainThread;
-// 	resetTimer();
-// 	getcontext(&(currentlyRunningThread->threadInfo->threadContext));
-// }
-
-
-// // adds blocks to the head of the reayQueue
-// // when we search for the next job we'll traverse the ready queue and select accordingly, so insertion is not too important
-// void addToReadyQueue(Node* thread) {
-// 	if(thread->threadInfo->threadID == 0)
-// 		return;
-// 	if(readyQueue == NULL) readyQueue = thread;
-// 	else {
-
-// 		Node* currentThread = readyQueue;
-// 		Node* prevThread = NULL;
-
-// 		while(currentThread != NULL) {
-// 			int currentThreadQuantum = currentThread->threadInfo->quantumsElapsed;
-// 			int incomingThreadQuantum = thread->threadInfo->quantumsElapsed;
-
-// 			if(incomingThreadQuantum < currentThreadQuantum) {
-// 				if(prevThread == NULL) { // the first quantum is greater than the incoming thread's, make head new thread
-// 					thread->next = readyQueue;
-// 					readyQueue = thread;
-// 				} else {
-// 					prevThread->next = thread;
-// 					thread->next = currentThread;
-// 				}
-// 				return;
-// 			}
-
-// 			prevThread = currentThread;
-// 			currentThread = currentThread->next;
-// 		}
-
-// 		// incoming thread has the greatest quantum of all elements in queue
-// 		prevThread->next = thread;
-// 	}
-
-// 	return;
-// }
-
-
-// /* create a new thread */
-// int mypthread_create(mypthread_t * thread, pthread_attr_t * attr,
-//                       void *(*function)(void*), void * arg) {
-//        // create Thread Control Block
-//        // create and initialize the context of this thread
-//        // allocate space of stack for this thread to run
-//        // after everything is all set, push this thread int
-//        // YOUR CODE HERE
-
-// 	  /*
-// 	  each thread is a represented by a thread control block. In other words,
-// 	  we're not thinking about individuals threads, but individuals control blocks
-
-// 	  each control block, which in this case is synonymous with a thread, has the following 
-// 	  attributes:
-
-// 	  1) threadId
-// 	  2) threadStatus
-// 	  3) context
-// 	  4)
-// 	  // add to readyQueue
-// 	  */
-
-// 	 // TODO: most likely have to account for main thread somehow
-// 	if(threadID == 0) runMainThread();
-// 	threadControlBlock* newThread = malloc(sizeof(threadControlBlock));
-// 	newThread->threadID = threadID;
-// 	newThread->threadStatus = READY;
-// 	newThread->sleptByThreadID = -1;
-// 	newThread->quantumsElapsed = 0;
-// 	newThread->valuePtr = NULL;
-// 	newThread->returnValue = NULL;
-// 	*thread = threadID++;
-// 	Node* newBlock = malloc(sizeof(Node));
-// 	newBlock->next = NULL;
-// 	newBlock->threadInfo = newThread;
-// 	addToReadyQueue(newBlock); // doesnt work yet, function doesnt work yet
-
-// 	getcontext(&(newThread->threadContext));
-// 	newThread->threadContext.uc_link = NULL;
-// 	newThread->threadContext.uc_stack.ss_sp = malloc(SIGSTKSZ);
-// 	newThread->threadContext.uc_stack.ss_size = SIGSTKSZ;
-// 	makecontext(&(newThread->threadContext), (void*) function, 1, arg);
-//     return 0;
-// }
-
-
-
-
-// // gets job with lowest QUANTUMelapsed, lowest elapsedQuantum indicates the thread that has ran the least
-// Node* getNextReadyThread() {
-// 	if(readyQueue == NULL) return NULL;
-// 	else {
-// 		// return the current head, lowest quantum
-// 		Node* lowestQuantumThread = readyQueue;
-// 		readyQueue = readyQueue->next;
-// 		return lowestQuantumThread;
-// 	}
-// }
-
-
-// /* give CPU possession to other user-level threads voluntarily: Done */
-// int mypthread_yield() {
-
-// 	// change thread state from Running to Ready
-// 	// save context of this thread to its thread control block
-// 	// wwitch from thread context to scheduler context
-// 	schedule();
-//     return 0;
-// };
-
-// void addToFinishedQueue(Node* currentThread) {
-// 	if(finishedQueue == NULL) finishedQueue = currentThread;
-// 	else {
-// 		currentThread->next = finishedQueue;
-// 		finishedQueue = currentThread;
-// 	}
-// }
-
-// void removeFromFinishedQueue(mypthread_t threadID) {
-// 	if(finishedQueue == NULL) return;
-
-// 	Node* prevThread = NULL;
-// 	Node* currentThread = finishedQueue;
-
-// 	while(currentThread != NULL) {
-// 		if(currentThread->threadInfo->threadID == threadID) {
-// 			if(prevThread == NULL) finishedQueue = finishedQueue->next;
-// 			else {
-// 				prevThread->next = currentThread->next;
-// 			}
-// 			return;
-// 		}
-
-// 		prevThread = currentThread;
-// 		currentThread = currentThread->next;
-// 	}
-// }
-
-// void addToFree(Node* currentThread) {
-// 	if(freeThreads == NULL) finishedQueue = currentThread;
-// 	else {
-// 		currentThread->next = freeThreads;
-// 		freeThreads = currentThread;
-// 	}
-// }
-
-// void addToSleepingQueue(Node* currentThread) {
-// 	if(sleepingQueue == NULL) sleepingQueue = currentThread;
-// 	else {
-// 		currentThread->next = sleepingQueue;
-// 		sleepingQueue = currentThread;
-// 	}
-// }
-
-// Node* getFromFinishedQueue(mypthread_t threadID) {
-// 	if(finishedQueue == NULL) return NULL;
-
-// 	Node* currentThread = finishedQueue;
-// 	while(currentThread != NULL) {
-// 		if(currentThread->threadInfo->threadID == threadID) return currentThread;
-
-// 		currentThread = currentThread->next;
-// 	}
-
-// 	return NULL;
-// }
-
-// Node* getFromSleepingQueue(mypthread_t threadID) {
-// 	if(sleepingQueue == NULL) return NULL;
-
-// 	Node* currentThread = sleepingQueue;
-// 	while(currentThread != NULL) {
-// 		if(currentThread->threadInfo->threadID == threadID) return currentThread;
-
-// 		currentThread = currentThread->next;
-// 	}
-
-// 	return NULL;
-// }
-
-// Node* getFromReadyQueue(mypthread_t threadID) {
-// 	if(readyQueue == NULL) return NULL;
-
-// 	Node* currentThread = readyQueue;
-// 	while(currentThread != NULL) {
-// 		if(currentThread->threadInfo->threadID == threadID) return currentThread;
-
-// 		currentThread = currentThread->next;
-// 	}
-
-// 	return NULL;
-// }
-
-// void wakeUpThread(mypthread_t threadID) {
-// 	if(sleepingQueue == NULL) return;
-
-// 	Node* currentThread = sleepingQueue;
-// 	Node* prevThread = NULL;
-// 	while(currentThread != NULL) {
-// 		if(currentThread->threadInfo->sleptByThreadID == threadID) {
-// 			if(prevThread == NULL) {
-// 				sleepingQueue = sleepingQueue->next;
-// 			} else {
-// 				prevThread->next = currentThread->next;
-// 			}
-// 			currentThread->threadInfo->sleptByThreadID = -1;
-// 			addToReadyQueue(currentThread);
-// 			return;
-// 		}
-
-// 		prevThread = currentThread;
-// 		currentThread = currentThread->next;
-// 	}
-
-// 	return;
-// }
-
-
-// /* terminate a thread */
-// void mypthread_exit(void *value_ptr) {
-// 	// Deallocated any dynamic memory created when starting this thread
-// 	// YOUR CODE HERE
-// 	if(currentlyRunningThread->threadInfo->valuePtr != NULL) {
-// 		*currentlyRunningThread->threadInfo->valuePtr = value_ptr;
-// 		currentlyRunningThread->threadInfo->threadStatus = FREE;
-// 		addToFree(currentlyRunningThread);
-// 	} else {
-// 		currentlyRunningThread->threadInfo->returnValue = value_ptr;
-// 		currentlyRunningThread->threadInfo->threadStatus = FINISHED;
-// 		addToFinishedQueue(currentlyRunningThread);
-// 	}
-
-// 	wakeUpThread(currentlyRunningThread->threadInfo->threadID);
-// 	schedule();
-// };
-
-
-// /* Wait for thread termination */
-// int mypthread_join(mypthread_t thread, void **value_ptr) {
-
-// 	// wait for a specific thread to terminate
-// 	// de-allocate any dynamic memory created by the joining thread
-
-// 	// YOUR CODE HERE
-// 	Node* threadToJoin = getFromFinishedQueue(thread);
-// 	if(threadToJoin != NULL) {
-// 		if(value_ptr != NULL) {
-// 			removeFromFinishedQueue(thread);
-// 			threadToJoin->threadInfo->threadStatus = FREE;
-// 			addToFree(threadToJoin);
-// 			*value_ptr = threadToJoin->threadInfo->returnValue;
-// 		}
-
-// 		return EXIT_SUCCESS;
-// 	}
-
-// 	currentlyRunningThread->threadInfo->threadStatus = SLEEP;
-// 	currentlyRunningThread->threadInfo->sleptByThreadID = thread;
-
-// 	Node* newSleepingThread = malloc(sizeof(Node));
-// 	newSleepingThread->next = NULL;
-// 	newSleepingThread->threadInfo = currentlyRunningThread->threadInfo;
-// 	addToSleepingQueue(newSleepingThread);
-
-// 	threadToJoin = getFromReadyQueue(thread);
-// 	if(threadToJoin != NULL) {
-// 		threadToJoin->threadInfo->valuePtr = value_ptr;
-// 	}
-// 	threadToJoin = getFromSleepingQueue(thread);
-// 	if(threadToJoin != NULL) {
-// 		threadToJoin->threadInfo->valuePtr = value_ptr;
-// 	}
-
-// 	schedule();
-// 	return EXIT_SUCCESS;
-// }
-
-// /* initialize the mutex lock */
-// int mypthread_mutex_init(mypthread_mutex_t *mutex,
-//                           const pthread_mutexattr_t *mutexattr) {
-// 	//initialize data structures for this mutex
-// 	if(threadID == 0) runMainThread();
-// 	mutex->waitList = NULL;
-// 	mutex->lock = 0;
-//     return EXIT_SUCCESS;
-// };
-
-// /* aquire the mutex lock */
-// int mypthread_mutex_lock(mypthread_mutex_t *mutex) {
-//         // use the built-in test-and-set atomic function to test the mutex
-//         // if the mutex is acquired successfully, enter the critical section
-//         // if acquiring mutex fails, push current thread into block list and //
-//         // context switch to the scheduler thread
-// 		while(atomic_flag_test_and_set(&(mutex->lock))){
-// 			currentlyRunningThread->threadInfo->threadStatus = WAITING;
-
-// 			Node* newThreadWaiting = malloc(sizeof(Node));
-// 			newThreadWaiting->next = mutex->waitList;
-// 			newThreadWaiting->threadInfo = currentlyRunningThread->threadInfo;
-// 			mutex->waitList = newThreadWaiting;
-// 			schedule();
-// 		}
-//         return 0;
-// };
-
-// /* release the mutex lock */
-// int mypthread_mutex_unlock(mypthread_mutex_t *mutex) {
-// 	// Release mutex and make it available again.
-// 	// Put threads in block list to run queue
-// 	// so that they could compete for mutex later.
-
-// 	// YOUR CODE HERE
-// 	while(mutex->waitList != NULL) {
-// 		addToReadyQueue(mutex->waitList);
-// 		mutex->waitList = mutex->waitList->next;
-// 	}
-// 	mutex->waitList = NULL;
-// 	mutex->lock = 0;
-// 	return 0;
-// };
-
-
-// /* destroy the mutex */
-// int mypthread_mutex_destroy(mypthread_mutex_t *mutex) {
-// 	// Deallocate dynamic memory created in mypthread_mutex_init
-// 	return 0;
-// };
-
-// /* scheduler: Done */
-// static void schedule() {
-// 	sched_stcf();
-// }
-
-// void cleanFree() {
-// 	if(freeThreads == NULL) return;
-
-// 	Node* prevThread = NULL;
-// 	Node* currentThread = freeThreads;
-
-// 	while(currentThread != NULL) {
-// 		prevThread = currentThread;
-// 		currentThread = currentThread->next;
-
-// 		//free prev node
-// 		free(prevThread->threadInfo->threadContext.uc_stack.ss_sp);
-// 		free(prevThread->threadInfo);
-// 		free(prevThread);
-// 	}
-
-// 	if(prevThread != NULL) {
-// 		free(prevThread->threadInfo->threadContext.uc_stack.ss_sp);
-// 		free(prevThread->threadInfo);
-// 		free(prevThread);
-// 	}
-
-// 	freeThreads = NULL;
-// }
-
-// /* Preemptive SJF (STCF) scheduling algorithm: Done */
-// static void sched_stcf() {
-// 	// Your own implementation of STCF
-// 	// (feel free to modify arguments and return types)
-// 	cleanFree();
-// 	signal(SIGPROF, SIG_IGN);
-// 	Node* nextThreadToRun = getNextReadyThread();
-
-// 	// there isn't another thread ready to run, so keep running the same one
-// 	if(nextThreadToRun == NULL) {
-// 		return ;
-// 	}
-
-// 	// at this point there IS another thread to yield to, so we increment the current thread's quantum (times it ran)
-// 	currentlyRunningThread->threadInfo->quantumsElapsed++;
-
-// 	// change the state of the thread that was currently running to ready and add it to the ready queue
-// 	currentlyRunningThread->threadInfo->threadStatus = READY;
-// 	addToReadyQueue(currentlyRunningThread);
-
-// 	// change the new thread's state to running;
-// 	nextThreadToRun->threadInfo->threadStatus = RUNNING;
-// 	currentlyRunningThread = nextThreadToRun;
-// 	resetTimer();
-// 	swapcontext(&(currentlyRunningThread->threadInfo->threadContext), &(nextThreadToRun->threadInfo->threadContext));
-// }
-
-// void destroyAll() {
-// 	if(readyQueue == NULL) return;
-
-// 	Node* currentNode = readyQueue;
-// 	while(currentNode != NULL) {
-// 		Node* prevThread = currentNode;
-// 		currentNode = currentNode->next;
-
-// 		fprintf(stdout, "%d", prevThread->threadInfo->threadID);
-// 		fflush(stdout);
-// 		free(prevThread->threadInfo->threadContext.uc_stack.ss_sp);
-// 		free(prevThread->threadInfo);
-// 		free(prevThread);
-// 	}
-// 	if(currentlyRunningThread != NULL){
-// 		free(currentlyRunningThread->threadInfo->threadContext.uc_stack.ss_sp);
-// 		free(currentlyRunningThread->threadInfo);
-// 		free(currentlyRunningThread);
-// 	}
-// }
--- mypthread.h
+++ mypthread.h
@@ -52,6 +52,20 @@ typedef struct mypthread_mutex_t {
 
 
 /* Function Declarations: */
+void enqueue(tcb* item);
+tcb* dequeue();
+void unblockThread(mypthread_t tid);
+bool isFinished(mypthread_t tid);
+tcb* getTCB(mypthread_t tid);
+void freeNode(threadNode* t);
+void destroyAll();
+void freeThreads(void);
+static void sched_stcf();
+tcb* create_tcb(mypthread_t tid, bool createContext);
+void resetTimer();
+void createMainThread();
+void freeThreads(void);
+
 /* create a new thread */
 int mypthread_create(mypthread_t * thread, pthread_attr_t * attr, void
     *(*function)(void*), void * arg);
@@ -93,173 +107,3 @@ void mainThreadAdd();
 #endif
 
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // File:    mypthread_t.h
-
-// // List all group member's name:
-// // username of iLab:
-// // iLab Server:
-
-// #ifndef MYTHREAD_T_H
-// #define MYTHREAD_T_H
-
-// #define _GNU_SOURCE
-
-// /* To use Linux pthread Library in Benchmark, you have to comment the USE_MYTHREAD macro */
-// #define USE_MYTHREAD 1
-
-// #define QUANTUM 15000;
-
-// /* include lib header files that you need here: */
-// #include <unistd.h>
-// #include <sys/syscall.h>
-// #include <sys/types.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <sys/time.h>
-// #include <signal.h>
-// #include <string.h>
-// #include <ucontext.h>
-// #include <stdatomic.h>
-// #include <ucontext.h>
-// #include <signal.h>
-// #include <stdbool.h>
-// #include <sys/time.h>
-// #include <stdatomic.h>
-// #include <string.h>
-
-// typedef unsigned int mypthread_t;
-
-// typedef enum {
-//     run, block, done, destroy
-// }mypthread_status;
-// typedef enum {
-//     READY,
-//     RUNNING,
-//     FINISHED,
-//     SLEEP,
-//     FREE,
-//     WAITING
-// } threadStatus;
-
-// typedef struct threadControlBlock {
-//     /* add important states in a thread control block */
-//     // thread Id
-//     // thread status
-//     // thread context
-//     // thread stack
-//     // thread priority
-//     // And more ...
-//     mypthread_t threadID;
-//     threadStatus threadStatus;
-//     ucontext_t threadContext;
-//     int quantumsElapsed;
-//     mypthread_t sleptByThreadID;
-//     void* returnValue;
-//     void** valuePtr;
-// }threadControlBlock;
-
-// /* define your data structures here: */
-// // Feel free to add your own auxiliary data structures (linked list or queue etc...)
-
-// typedef struct Node {
-//     threadControlBlock* threadInfo;
-//     struct Node* next;
-// } Node;
-
-
-// /* mutex struct definition */
-// typedef struct mypthread_mutex_t {
-//     /* add something here */
-//     Node* waitList;
-//     int lock;
-// } mypthread_mutex_t;
-
-// // typedef struct mutexNode {
-// //       mypthread_mutex_t* mutex;
-// //       struct mutexNode* next;
-// // } mutexNode;
-
-// // YOUR CODE HERE
-
-
-// /* Function Declarations: */
-
-
-
-// /* create a new thread */
-// int mypthread_create(mypthread_t * thread, pthread_attr_t * attr, void
-//     *(*function)(void*), void * arg);
-
-// /* give CPU pocession to other user level threads voluntarily */
-// int mypthread_yield();
-
-// /* terminate a thread */
-// void mypthread_exit(void *value_ptr);
-
-// /* wait for thread termination */
-// int mypthread_join(mypthread_t thread, void **value_ptr);
-
-// /* initial the mutex lock */
-// int mypthread_mutex_init(mypthread_mutex_t *mutex, const pthread_mutexattr_t
-//     *mutexattr);
-
-// /* aquire the mutex lock */
-// int mypthread_mutex_lock(mypthread_mutex_t *mutex);
-
-// /* release the mutex lock */
-// int mypthread_mutex_unlock(mypthread_mutex_t *mutex);
-
-// /* destroy the mutex */
-// int mypthread_mutex_destroy(mypthread_mutex_t *mutex);
-
-// #ifdef USE_MYTHREAD
-// #define pthread_t mypthread_t
-// #define pthread_mutex_t mypthread_mutex_t
-// #define pthread_create mypthread_create
-// #define pthread_exit mypthread_exit
-// #define pthread_join mypthread_join
-// #define pthread_mutex_init mypthread_mutex_init
-// #define pthread_mutex_lock mypthread_mutex_lock
-// #define pthread_mutex_unlock mypthread_mutex_unlock
-// #define pthread_mutex_destroy mypthread_mutex_destroy
-// #endif
-
-// #endif
